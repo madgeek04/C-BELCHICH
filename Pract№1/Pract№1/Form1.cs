f51f799d0665eafebd2bc321230a0f48f40aca03
madgeek04 <95250794+madgeek04@users.noreply.github.com>
2023-01-30 13:02:09 +0300

    Практическая №1

---
+++ Pract№1/Pract№1/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+using System.Windows.Forms;
+
+namespace Pract_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Enabled)
+            {
+                button2.Enabled = false;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (button2.Enabled)
+            {
+                button3.Enabled = false;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (button3.Enabled)
+            {
+                button4.Enabled = false;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (button4.Enabled)
+            {
+                button5.Enabled = false;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (button5.Enabled)
+            {
+                button6.Enabled = false;
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (button6.Enabled)
+            {
+                button7.Enabled = false;
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (button7.Enabled)
+            {
+                button8.Enabled = false;
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (button8.Enabled)
+            {
+                button9.Enabled = false;
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (button9.Enabled)
+            {
+                button1.Enabled = false;
+            }
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+            button2.Enabled = true;
+            button3.Enabled = true;
+            button4.Enabled = true;
+            button5.Enabled = true;
+            button6.Enabled = true;
+            button7.Enabled = true;
+            button8.Enabled = true;
+            button9.Enabled = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

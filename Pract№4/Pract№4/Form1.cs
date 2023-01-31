14f08fa4e6855493b699194eb22b70d253bda025
madgeek04 <95250794+madgeek04@users.noreply.github.com>
2023-01-31 06:42:04 +0300

    Практическая №4

---
+++ Pract№4/Pract№4/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Pract_4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double x0 = Convert.ToDouble(textBox1.Text);
+            double xk = Convert.ToDouble(textBox2.Text);
+            double dx = Convert.ToDouble(textBox3.Text);
+            double a = Convert.ToDouble(textBox4.Text);
+            double x = x0;
+            while (x <= (xk + dx / 2.0))
+            {
+                double y = (Math.Pow(x, (5.0 / 2.0)) - a) * Math.Log(Math.Pow(x, 2) + 12, 7);
+                textBox5.Text += "x=" + Convert.ToString(x) + "; y=" + Convert.ToString(y) + Environment.NewLine;
+                x += dx;
+            }
+        }
+    }
+}
